26284c537e6641da0b2c26e61425a016606f76f6
ProstoJohn <bogdanchik10@gmail.com>
2016-01-26 12:49:08 +0200

    Update structure

--- js/commits.js
+++ js/commits.js
@@ -1,14 +1,14 @@
-function reposvar(thiser) {
-  var data = JSON.parse($(thiser).closest('li').attr('data-json'));
+function reposvar(commitsList) {
+  var data = JSON.parse($(commitsList).closest('li').attr('data-json'));
   var xhr4 = new XMLHttpRequest();
   xhr4.open('GET', 'https://api.github.com/repos/' + data['login'] + '/' + data['name'] + '/commits', false);
   xhr4.send();
   var commitlist = JSON.parse(xhr4.responseText);
   JSON.stringify(commitlist);
-  $(thiser).closest('li').append('<br><p><font color="blue">Имя создателя: </font>' + data['login'] + '</p><font color="red" size="7">Список коммитов:</font>');
+  $(commitsList).closest('li').append('<br><p><font color="blue">Имя создателя: </font>' + data['login'] + '</p><font color="red" size="7">Список коммитов:</font>');
   var spcommit;
   for (var i = 0; i < commitlist.length; i++) {
     spcommit = '<hr><div><b>Коммит: </b>' + commitlist[i]['commit']['message'] + '<br> <b>Пользователь оставивший коммит: ' + commitlist[i]['committer']['login'] + '</b></div>';
-    $(thiser).closest('li').append(spcommit);
+    $(commitsList).closest('li').append(spcommit);
   }
 }
--- js/forks.js
+++ js/forks.js
@@ -1,5 +1,5 @@
-function reposforks(thiser) {
-  var data = JSON.parse($(thiser).closest('li').attr('data-json'));
+function reposforks(forksList) {
+  var data = JSON.parse($(forksList).closest('li').attr('data-json'));
   var xhr5 = new XMLHttpRequest();
   xhr5.open('GET', 'https://api.github.com/repos/' + data['login'] + '/' + data['name'] + '/forks', false);
   xhr5.send();
@@ -7,9 +7,9 @@ function reposforks(thiser) {
   JSON.stringify(forkslist);
   var username;
   username = '<br><font color="red" size="7">Список форков:</font>';
-  $(thiser).closest('li').append(username);
+  $(forksList).closest('li').append(username);
   for (var i = 0; i < forkslist.length; i++) {
     spforks = '<hr><div>Пользователь оставивший форк: ' + forkslist[i]['owner']['login'] + '</div>';
-    $(thiser).closest('li').append(spforks);
+    $(forksList).closest('li').append(spforks);
   }
 }
